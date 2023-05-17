47ba92f8456501df472accbee9de974b198c29a6
Erin Alexander <erin.alexander@tempus-ex.com>
2023-05-17 11:52:56 -0700

    header setup

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,17 +1,19 @@
-import { Header as MantineHeader } from '@mantine/core';
-import Image from 'next/image';
-import Link from 'next/link';
+import Link from "next/link";
 
-import styles from './styles.module.scss';
-import logo from '../../public/images/logo.svg';
+import styles from "./styles.module.scss";
 
 export const Header = () => {
-    return (
-        <MantineHeader className={styles.header} height={64}>
-            <Link className={styles['home-link']} href="/">
-                <Image alt="Tempus Ex" className={styles.logo} src={logo} />
-                <span>Tempus Ex Docs</span>
-            </Link>
-        </MantineHeader>
-    )
+  return (
+    <header className={styles.header}>
+      <div className={styles["header__inner"]}>
+        <Link className={styles["header__link"]} href="/">
+          <img
+            alt="Tempus Ex"
+            className={styles["header__logo"]}
+            src={"images/FusionFeedLogo.svg"}
+          />
+        </Link>
+      </div>
+    </header>
+  );
 };

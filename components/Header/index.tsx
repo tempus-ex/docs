193d97570a5401b1d1a3894e08f4ed3b36fe7d86
Erin Alexander <erin.alexander@tempus-ex.com>
2023-05-23 10:35:44 -0700

    added quick darkmode toggle for testing

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 import styles from "./styles.module.scss";
 
 export const Header = () => {
+
+  const toggleDarkMode = () => {
+    document.body.toggleAttribute("data-darkmode");
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles["header__inner"]}>
@@ -13,6 +18,11 @@ export const Header = () => {
             src={"/images/FusionFeedLogo.svg"}
           />
         </Link>
+        {/**TODO:  This dark mode toggle is for testing only. Remove before deploy */}
+        <label>
+          Dark Mode
+          <input type="checkbox" onChange={toggleDarkMode} />
+        </label>
       </div>
     </header>
   );

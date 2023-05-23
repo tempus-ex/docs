ffc9a351f7065da68e24c9f2aeca545a331ef1d6
Erin Alexander <erin@hypothete.com>
2023-05-23 10:50:23 -0700

    Landing page styling and general theme work (#8)
    * product cards
    
    * missed images and connecting to border color
    
    * messing with heading styles
    
    * added quick darkmode toggle for testing

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,6 +3,11 @@ import Link from "next/link";
 import styles from "./styles.module.scss";
 
 export const Header = () => {
+
+  const toggleDarkMode = () => {
+    document.body.toggleAttribute("data-darkmode");
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles["header__inner"]}>
@@ -10,9 +15,14 @@ export const Header = () => {
           <img
             alt="Tempus Ex"
             className={styles["header__logo"]}
-            src={"images/FusionFeedLogo.svg"}
+            src={"/images/FusionFeedLogo.svg"}
           />
         </Link>
+        {/**TODO:  This dark mode toggle is for testing only. Remove before deploy */}
+        <label>
+          Dark Mode
+          <input type="checkbox" onChange={toggleDarkMode} />
+        </label>
       </div>
     </header>
   );

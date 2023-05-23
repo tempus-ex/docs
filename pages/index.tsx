2438373ac7fe3b7f720d6f1cccb3466353831d7e
Erin Alexander <erin@hypothete.com>
2023-05-23 10:50:23 -0700

    Landing page styling and general theme work (#8)
    * product cards
    
    * missed images and connecting to border color
    
    * messing with heading styles
    
    * added quick darkmode toggle for testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,49 +1,11 @@
-import { HomePage, Props } from "../components/HomePage";
+import { HomePage } from "../components/HomePage";
 import { withAuth } from "../lib/auth";
-import { canonicalContentPath, Frontmatter } from "../lib/content";
-import { getAllContent } from "../lib/content-loading";
 
 export default HomePage;
 
-interface ProductFrontmatter {
-  path: string;
-  icon: string;
-  description: string;
-  children: string[];
-}
-
-interface HomePageFrontmatter extends Frontmatter {
-  products: ProductFrontmatter[];
-}
-
-export const getServerSideProps = withAuth<{}, Props>(async function () {
-  const allContent = await getAllContent();
-  const content = allContent.get("/")!;
-  const frontmatter = content.frontmatter as unknown as HomePageFrontmatter;
+export const getServerSideProps = withAuth(async function () {
 
   return {
-    props: {
-      products: frontmatter.products.map((p) => {
-        const path = canonicalContentPath(p.path);
-        const content = allContent.get(path)!;
-
-        return {
-          path,
-          icon: p.icon,
-          title: content.frontmatter.title,
-          description: p.description,
-          children:
-            p.children?.map((c) => {
-              const path = canonicalContentPath(c);
-              const content = allContent.get(path)!;
-
-              return {
-                path,
-                title: content.frontmatter.title,
-              };
-            }) || [],
-        };
-      }),
-    },
+    props: {},
   };
 });

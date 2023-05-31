bfc69b5d343e1ee4912ade92ab74cff25bfeff7a
Erin Alexander <erin@hypothete.com>
2023-05-31 12:05:01 -0700

    Cc 19 sign in page styling error messaging (#10)
    * wip signin
    
    * styled key input
    
    * added login logo
    
    * made alt footer
    
    * splitting up login states
    
    * fixed responsiveness bug and added cookie expiry
    
    * adding license agreement state
    
    * added license agreement as pdf
    
    * minor tweaks
    
    * lockfile trickiness
    
    * fixed links

---
+++ components/LoginPage/LoginAgreement.tsx
@@ -0,0 +1,48 @@
+import clsx from "clsx";
+import cookie from "js-cookie";
+import { useRouter } from "next/router";
+import styles from "./styles.module.scss";
+
+export type LoginAgreementProps = {
+  setLoginState: (formState: "login" | "agreement") => void;
+};
+
+export const LoginAgreement = ({ setLoginState }: LoginAgreementProps) => {
+  const router = useRouter();
+
+  const declineAgreement = () => {
+    cookie.remove("ffcookie");
+    setLoginState("login");
+  };
+
+  const acceptAgreement = () => {
+    cookie.set("ffagreement", "true");
+    const destination = new URLSearchParams(window.location.search).get(
+      "destination"
+    );
+    router.push(destination || "/");
+  };
+
+  return (
+    <>
+      <iframe
+        className={styles["agreement__iframe"]}
+        src={`./license-agreement.pdf#toolbar=0&navpanes=0&scrollbar=0&statusbar=0&messages=0&scrollbar=0`}
+      ></iframe>
+      <div className={styles["agreement__button-row"]}>
+        <button
+          onClick={declineAgreement}
+          className={clsx({
+            [styles["button--bordered"]]: true,
+            [styles["agreement__decline-button"]]: true,
+          })}
+        >
+          Decline
+        </button>
+        <button onClick={acceptAgreement} className={styles["button"]}>
+          Accept
+        </button>
+      </div>
+    </>
+  );
+};
---
+++ components/LoginPage/LoginFooter.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+export const LoginFooter = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles['footer__links']}>
+        <Link
+          className={styles["footer__link"]}
+          href="https://tempus-ex.com/"
+        >
+          Home
+        </Link>
+        <Link
+          className={styles["footer__link"]}
+          href="./license-agreement.pdf"
+          target="blank"
+        >
+          Terms & Privacy
+        </Link>
+        <Link
+          className={styles["footer__link"]}
+          href="mailto:contact@tempus-ex.com"
+        >
+          Contact
+        </Link>
+      </div>
+      <p className={styles["footer__patent"]}>
+        Protected by U.S. Patent Nos. 11,140,328 & 11,172,248
+      </p>
+    </footer>
+  );
+};

3b6accb56c9bda3076061b78141593f5ec58dc46
ccbrown <chris@tempus-ex.com>
2023-05-31 20:06:53 -0400

    add rest explorer

---
+++ pages/fusionfeed/rest/explorer.tsx
@@ -0,0 +1,19 @@
+import { RESTExplorerPage, Props } from '../../../components/RESTExplorerPage';
+import { withAuth } from '../../../lib/auth';
+import { getAllContent } from '../../../lib/content-loading';
+
+export default RESTExplorerPage;
+
+export const getServerSideProps = withAuth<{}, Props>(async function ({ params }) {
+    const allContent = await getAllContent();
+    const content = allContent.get(`/fusionfeed/rest/explorer`);
+    if (!content) {
+        return { notFound: true };
+    }
+
+    return {
+        props: {
+            source: content.source,
+        },
+    };
+});

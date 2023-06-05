254075b72005bfcad48646ca9d30ba2ed23611eb
Chris <chris@tempus-ex.com>
2023-06-05 13:01:23 -0400

    start sport-specific docs (#13)
    * start sport-specific docs
    
    * fix root canonicalization

--- pages/fusionfeed/rest/explorer-and-reference.tsx
+++ pages/fusionfeed/rest/explorer-and-reference.tsx
@@ -1,12 +1,12 @@
-import { GraphQLExplorerPage, Props } from '../../../components/GraphQLExplorerPage';
+import { RESTExplorerPage, Props } from '../../../components/RESTExplorerPage';
 import { withAuth } from '../../../lib/auth';
 import { getAllContent } from '../../../lib/content-loading';
 
-export default GraphQLExplorerPage;
+export default RESTExplorerPage;
 
 export const getServerSideProps = withAuth<{}, Props>(async function ({ params }) {
     const allContent = await getAllContent();
-    const content = allContent.get(`/fusionfeed/graphql/explorer`);
+    const content = allContent.get(`/fusionfeed/rest/explorer-and-reference`);
     if (!content) {
         return { notFound: true };
     }

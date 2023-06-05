d310fc2dc0d0929e3e42a0575683a53dd8cf847e
Robin Lake <robin@tempus-ex.com>
2023-06-05 10:26:21 -0700

    cookie behaviour policy, results in 502 from cloudfront when attempting to log in

--- aws/lib/docs-stack.ts
+++ aws/lib/docs-stack.ts
@@ -22,68 +22,28 @@ export class DocsStack extends cdk.Stack {
     const {domainName, subDomainName, certificateArn} = props;
     const fullSubdomain = [subDomainName, domainName].join(".");
 
-    // defines an AWS Lambda resource
-    // const hello = new lambda.Function(this, 'HelloHandler', {
-    //   // runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
-    //   runtime: lambda.Runtime.FROM_IMAGE,    // execution environment
-    //   // code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-    //   // code: lambda.Code.fromAsset('../.next'),  // code loaded from "lambda" directory
-    //   // code: lambda.Code.fromDockerBuild(".."),  // code loaded from "lambda" directory
-    //   code: lambda.Code.fromDockerBuild("..", {imagePath: "/app"}),  // code loaded from "lambda" directory
-    //   // handler: 'hello.handler'                // file is "hello", function is "handler"
-    //   handler: lambda.Handler.FROM_IMAGE                // file is "hello", function is "handler"
-    // });
-
     const domainCert = certificatemanager.Certificate.fromCertificateArn(this, 'DomainCert', certificateArn);
 
     const docsHandler = new lambda.DockerImageFunction(this, 'DocsHandler', {
       code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../..')),
     });
 
-
-
-
-    // defines an API Gateway REST API resource backed by our "hello" function.
     const api = new apigateway.LambdaRestApi(this, 'DocsEndpoint', {
       handler: docsHandler,
-      // proxy: false,
-      // domainName: {
-      //   domainName: fullSubdomain,
-      //   certificate: domainCert,
-      // },
     });
-
-    // const login = api.root.addResource('login');
-    // login.addMethod('GET');   
-    // const root = api.root.addResource('/');
-    // root.addMethod('GET');   
-
-
-    // const apiDomainName = apigateway.DomainName.fromDomainNameAttributes(this, 'DocsDomain', {
-    //   domainName: fullSubdomain,
-    //   domainNameAliasHostedZoneId: 'Z01264622EAQBCH25W57L',
-    //   domainNameAliasTarget: 'prod',
-    // });
-    
-    // new apigateway.BasePathMapping(this, 'BasePathMapping', {
-    //   domainName: apiDomainName,
-    //   restApi: api,
-    // });
 
     // const cachePolicy = new CachePolicy(this, "CachePolicy", {
     //   headerBehavior: CacheHeaderBehavior.allowList("Authorization"),
     // });
 
-    // const originRequestPolicy = new OriginRequestPolicy(this, 'RequestPolicy', {
-    //   // queryStringBehavior: OriginRequestQueryStringBehavior.all(),
-    //   // cookieBehavior: OriginRequestCookieBehavior.all(),
-    // })
+    const originRequestPolicy = new OriginRequestPolicy(this, 'RequestPolicy', {
+      // queryStringBehavior: OriginRequestQueryStringBehavior.all(),
+      cookieBehavior: OriginRequestCookieBehavior.all(),
+    })
 
     const logsBucket = new Bucket(this, "LogsBucket", {
       objectOwnership: ObjectOwnership.OBJECT_WRITER,
     });
-
-
 
     const distribution = new Distribution(this, 'DocsDistribution', {
       domainNames: [`${subDomainName}.${domainName}`],
@@ -93,7 +53,7 @@ export class DocsStack extends cdk.Stack {
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
         origin: new origins.RestApiOrigin(api),
         // cachePolicy,
-        // originRequestPolicy,
+        originRequestPolicy,
       },
     })
 
@@ -107,51 +67,5 @@ export class DocsStack extends cdk.Stack {
       target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution))
     })
 
-    // new route53.ARecord(this, 'a-record', {
-    //   recordName: subDomainName,
-    //   zone,
-    //   target: route53.RecordTarget.fromAlias({
-    //     bind() {
-    //       return {
-    //         dnsName: fullSubdomain, // Specify the applicable domain name for your API.,
-    //         hostedZoneId: 'Z01264622EAQBCH25W57L', // Specify the hosted zone ID for your API.
-    //       };
-    //     },
-    //     },)
-    // })
-
   }
 }
-
-
-// import * as cdk from 'aws-cdk-lib';
-// import { Construct } from 'constructs';
-// import {
-//   Certificate,
-//   CertificateValidation,
-// } from "aws-cdk-lib/aws-certificatemanager";
-// // import * as sqs from 'aws-cdk-lib/aws-sqs';
-// import { ARecord, HostedZone, RecordTarget } from "aws-cdk-lib/aws-route53";
-
-// interface DocsProps extends cdk.StackProps {
-//   domainName: string
-//   subDomainName: string
-//   certificateArn: string
-//   // bucketName: string
-// }
-
-// export class DocsStack extends cdk.Stack {
-//   constructor(scope: Construct, id: string, props: DocsProps) {
-//     super(scope, id, props);
-//     const {domainName, subDomainName, certificateArn} = props;
-//     const fullSubdomain = [domainName, subDomainName].join(".");
-
-//     const zone = HostedZone.fromLookup(this, "Zone", { domainName });
-
-//     const certificate = new Certificate(this, "Certificate", {
-//       domainName: fullSubdomain,
-//       validation: CertificateValidation.fromDns(zone),
-//     });
-
-//   }
-// }

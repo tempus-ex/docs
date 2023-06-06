92f912122571ef1edacd4df46a1d90583833623b
Robin Lake <robin@tempus-ex.com>
2023-06-06 07:45:15 -0700

    formatting

--- aws/lib/docs-stack.ts
+++ aws/lib/docs-stack.ts
@@ -1,42 +1,57 @@
-import * as path from 'path';
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as certificatemanager from 'aws-cdk-lib/aws-certificatemanager';
-import * as ecr_assets from 'aws-cdk-lib/aws-ecr-assets';
-import * as route53 from 'aws-cdk-lib/aws-route53';
-import * as targets from 'aws-cdk-lib/aws-route53-targets';
-import { CacheHeaderBehavior, CachePolicy, Distribution, OriginRequestCookieBehavior, OriginRequestPolicy, OriginRequestQueryStringBehavior, ViewerProtocolPolicy } from 'aws-cdk-lib/aws-cloudfront';
-import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
-import { Bucket, BucketAccessControl, ObjectOwnership } from 'aws-cdk-lib/aws-s3';
+import * as path from "path";
+import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as certificatemanager from "aws-cdk-lib/aws-certificatemanager";
+import * as ecr_assets from "aws-cdk-lib/aws-ecr-assets";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as targets from "aws-cdk-lib/aws-route53-targets";
+import {
+  CacheHeaderBehavior,
+  CachePolicy,
+  Distribution,
+  OriginRequestCookieBehavior,
+  OriginRequestPolicy,
+  OriginRequestQueryStringBehavior,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import {
+  Bucket,
+  BucketAccessControl,
+  ObjectOwnership,
+} from "aws-cdk-lib/aws-s3";
 
 interface DocsProps extends cdk.StackProps {
-  domainName: string
-  subDomainName: string
-  certificateArn: string
+  domainName: string;
+  subDomainName: string;
+  certificateArn: string;
 }
-
 
 export class DocsStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props: DocsProps) {
     super(scope, id, props);
-    const {domainName, subDomainName, certificateArn} = props;
+    const { domainName, subDomainName, certificateArn } = props;
     const fullSubdomain = [subDomainName, domainName].join(".");
 
-    const domainCert = certificatemanager.Certificate.fromCertificateArn(this, 'DomainCert', certificateArn);
+    const domainCert = certificatemanager.Certificate.fromCertificateArn(
+      this,
+      "DomainCert",
+      certificateArn
+    );
 
-    const docsHandler = new lambda.DockerImageFunction(this, 'DocsHandler', {
-      code: lambda.DockerImageCode.fromImageAsset(path.join(__dirname, '../..'), {
-        platform: ecr_assets.Platform.LINUX_AMD64,
-      }),
-        timeout: cdk.Duration.seconds(30),
+    const docsHandler = new lambda.DockerImageFunction(this, "DocsHandler", {
+      code: lambda.DockerImageCode.fromImageAsset(
+        path.join(__dirname, "../.."),
+        {
+          platform: ecr_assets.Platform.LINUX_AMD64,
+        }
+      ),
+      timeout: cdk.Duration.seconds(30),
     });
 
-
-
-
-    const api = new apigateway.LambdaRestApi(this, 'DocsEndpoint', {
-        binaryMediaTypes: ['*/*'],
+    const api = new apigateway.LambdaRestApi(this, "DocsEndpoint", {
+      binaryMediaTypes: ["*/*"],
       handler: docsHandler,
       domainName: {
         domainName: fullSubdomain,
@@ -44,14 +59,14 @@ export class DocsStack extends cdk.Stack {
       },
     });
 
-      const zone = route53.HostedZone.fromLookup(this, "zone", {
-        domainName,
-      });
+    const zone = route53.HostedZone.fromLookup(this, "zone", {
+      domainName,
+    });
 
-      new route53.ARecord(this, 'a-record', {
-        recordName: subDomainName,
-        zone,
-        target: route53.RecordTarget.fromAlias(new targets.ApiGateway(api)),
-      });
+    new route53.ARecord(this, "a-record", {
+      recordName: subDomainName,
+      zone,
+      target: route53.RecordTarget.fromAlias(new targets.ApiGateway(api)),
+    });
   }
 }

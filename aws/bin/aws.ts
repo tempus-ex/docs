3a6559a51b15ca94383ef4e4d59326196c4b0663
Robin Lake <robin@tempus-ex.com>
2023-06-06 14:09:10 -0700

    new cert

--- aws/bin/aws.ts
+++ aws/bin/aws.ts
@@ -4,18 +4,18 @@ import * as cdk from 'aws-cdk-lib';
 import { DocsStack } from '../lib/docs-stack';
 
 const demoArn = 'arn:aws:acm:us-east-1:992150767891:certificate/c5a6eb00-528f-473d-89a7-cb64b85722e9';
-const webProdArn = 'arn:aws:acm:us-east-1:189912143729:certificate/217bc40a-88c9-437b-adb7-394bca6d31f1';
+const webProdArn = 'arn:aws:acm:us-east-1:189912143729:certificate/a460ee40-07ed-4eee-b425-9bcf9303b15c';
 
 const app = new cdk.App();
-new DocsStack(app, 'DemoDocsStack', {
-    env: {
-        account: '992150767891',
-        region: 'us-east-1',
-    },
-    domainName: 'demo.tempus-ex.com',
-    subDomainName: 'docs',
-    certificateArn: demoArn,
-});
+// new DocsStack(app, 'DemoDocsStack', {
+//     env: {
+//         account: '992150767891',
+//         region: 'us-east-1',
+//     },
+//     domainName: 'demo.tempus-ex.com',
+//     subDomainName: 'docs',
+//     certificateArn: demoArn,
+// });
 
 new DocsStack(app, 'ProdDocsStack', {
     env: {

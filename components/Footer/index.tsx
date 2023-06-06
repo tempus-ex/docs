3d5922b32729e8104d9ee29eeeb107073a9e4adc
Erin Alexander <erin@hypothete.com>
2023-06-06 09:04:08 -0700

    Update components/Footer/index.tsx
    Co-authored-by: Chris <chris@tempus-ex.com>

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -17,7 +17,7 @@ export const Footer = () => {
       })}
     >
       <div className={styles["footer__inner"]}>
-        <h2 className={styles["footer__title"]}>Fusion Feed API</h2>
+        <h2 className={styles["footer__title"]}>FusionFeed API</h2>
         <div
           className={clsx([styles["footer__section"], styles["footer__links"]])}
         >

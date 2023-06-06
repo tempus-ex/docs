c92e287bd094559b73c98dee809d26643de6eb32
Erin Alexander <erin.alexander@tempus-ex.com>
2023-06-05 17:13:38 -0700

    fixed overflow issue and config missing

--- components/DefaultPage/index.tsx
+++ components/DefaultPage/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import getConfig from 'next/config';
 import Head from "next/head";
 import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
 
@@ -8,6 +9,10 @@ import { TableOfContents } from "../../lib/content";
 
 import styles from "./styles.module.scss";
 import { NavBar } from "./Navbar";
+
+const scope = {
+  config: getConfig(),
+};
 
 export interface Props {
   path: string;
@@ -28,7 +33,7 @@ export const DefaultPage = (props: Props) => {
       <main className={styles.main}>
           <NavBar toc={toc} path={props.path} />
           <div className={styles['generated-content']}>
-            <MDXRemote {...props.source} />
+            <MDXRemote {...props.source} scope={scope} />
           </div>
       </main>
       <Footer />

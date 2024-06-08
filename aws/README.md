9a7e2a23bb0e8f6ee958f521a83d970e8ba9c7ea
ccbrown <chris@tempus-ex.com>
2024-06-08 00:38:57 -0400

    continuous deployment on merge to main

--- aws/README.md
+++ aws/README.md
@@ -1,14 +1,11 @@
-# Welcome to your CDK TypeScript project
+# aws
 
-This is a blank project for CDK development with TypeScript.
+This is the CDK deployment for the docs stack.
 
-The `cdk.json` file tells the CDK Toolkit how to execute your app.
+## Deploying
 
-## Useful commands
+After installing dependencies with `npm install`, you can deploy the stack with the following command:
 
-* `npm run build`   compile typescript to js
-* `npm run watch`   watch for changes and compile
-* `npm run test`    perform the jest unit tests
-* `cdk deploy`      deploy this stack to your default AWS account/region
-* `cdk diff`        compare deployed stack with current state
-* `cdk synth`       emits the synthesized CloudFormation template
+```bash
+npx aws-cdk deploy ProdDocsStack
+```
--- aws/bin/aws.ts
+++ aws/bin/aws.ts
@@ -2,11 +2,13 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DocsStack } from '../lib/docs-stack';
+import { GitHubActionsStack } from '../lib/github-actions-stack';
 
-const demoArn = 'arn:aws:acm:us-east-1:992150767891:certificate/c5a6eb00-528f-473d-89a7-cb64b85722e9';
-const webProdArn = 'arn:aws:acm:us-east-1:189912143729:certificate/a460ee40-07ed-4eee-b425-9bcf9303b15c';
+const DEMO_CERTIFICATE_ARN = 'arn:aws:acm:us-east-1:992150767891:certificate/c5a6eb00-528f-473d-89a7-cb64b85722e9';
+const PROD_CERTIFICATE_ARN = 'arn:aws:acm:us-east-1:189912143729:certificate/a460ee40-07ed-4eee-b425-9bcf9303b15c';
 
 const app = new cdk.App();
+
 new DocsStack(app, 'DemoDocsStack', {
     env: {
         account: '992150767891',
@@ -14,15 +16,27 @@ new DocsStack(app, 'DemoDocsStack', {
     },
     domainName: 'demo.tempus-ex.com',
     subDomainName: 'docs',
-    certificateArn: demoArn,
+    certificateArn: DEMO_CERTIFICATE_ARN,
 });
 
-new DocsStack(app, 'ProdDocsStack', {
+const prodDocsStack = new DocsStack(app, 'ProdDocsStack', {
     env: {
         account: '189912143729',
         region: 'us-east-1',
     },
     domainName: 'docs.tempus-ex.com',
     subDomainName: '',
-    certificateArn: webProdArn,
+    certificateArn: PROD_CERTIFICATE_ARN,
+    synthesizer: new cdk.DefaultStackSynthesizer({
+        deployRoleArn: app.node.tryGetContext('deployRoleArn'),
+    }),
+});
+
+new GitHubActionsStack(app, 'ProdDocsGitHubActionsStack', {
+    branch: 'main',
+    docsStack: prodDocsStack,
+    env: {
+        account: '189912143729',
+        region: 'us-east-1',
+    },
 });
